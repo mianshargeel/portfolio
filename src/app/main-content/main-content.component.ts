8f67badb0804efb48e69329691767badc75d6b09
mian shargeel <call2poma@gmail.com>
2025-03-21 11:33:44 +0100

    Atf and Navbar creadted

---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { AtfComponent } from './atf/atf.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [AtfComponent,
+    NavbarComponent,
+    AboutmeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    TestimonialsComponent,
+    ContactComponent
+  ],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss'
+})
+export class MainContentComponent {
+
+}
