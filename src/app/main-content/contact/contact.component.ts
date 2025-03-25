dc0099f77e228a0c2072abd4fa794dcc19fed6ee
mian shargeel <call2poma@gmail.com>
2025-03-25 15:38:42 +0100

    Contact component is created, and functionality with validation also.

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+interface FormDate{
+  name: string;
+  email: string;
+  message: string;
+}
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  contactData: FormDate = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log('Form Submitted: ', this.contactData);
+    }
+    ngForm.resetForm();
+  }
 
 }
