c940f2314b82fa270ab50632c3cd51158cb35a89
mian shargeel <call2poma@gmail.com>
2025-04-03 17:50:24 +0200

    route corrected

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ContactComponent } from './main-content/contact/contact.component';
-import { PrivacyPolicyComponent } from './legalization/privacy-policy/privacy-policy.component';
 import { ImpressumComponent } from './legalization/impressum/impressum.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 
 export const routes: Routes = [
