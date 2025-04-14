5ae126b814aa1ff19f8ad08b724e45af17d09c17
mian shargeel <call2poma@gmail.com>
2025-04-14 19:18:15 +0200

    Optimized, some little issues.

--- src/app/legalization/impressum/impressum.component.ts
+++ src/app/legalization/impressum/impressum.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../main-content/navbar/navbar.component';
 import { TEXTS } from '../../constants/texts';
 import { LanguageService } from '../../services/language.service';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss'
 })
